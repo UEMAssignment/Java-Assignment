0d8733e05bc9d5f994ab68635f1cb3774da75024
NightMareRahul <nightmarecoding0001@gmail.com>
2024-03-14 08:45:46 +0000

    first commit

---
+++ week_8/Q_1.java
@@ -0,0 +1,30 @@
+package week_8;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Q_1 {
+    static int fact(int num) {
+        int fact = 1;
+        for (int i = num; i > 1; i--)
+            fact *= i;
+        return fact;
+    }
+    public static void main(String[] args) throws IOException{
+        System.out.print("Enter a Number(Input through BufferReader): ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num2 = Integer.parseInt(br.readLine());
+        int fact2 = Q_1.fact(num2);
+
+        System.out.print("Enter a Number(Input through Scanner class): ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
+        int fact1 = Q_1.fact(num);
+        
+        System.out.println("Factorial of " + num + " = " + fact1);
+        System.out.println("Factorial of " + num2 + " = " + fact2);
+    }
+}
