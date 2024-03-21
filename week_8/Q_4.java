b700504f00b2eca6e89f7b05ccfcb54d7d5274cb
NightMareRahul <nightmarecoding0001@gmail.com>
2024-03-21 10:40:56 +0530

    updated

--- week_8/Q_4.java
+++ week_8/Q_4.java
@@ -1,7 +1,17 @@
 package week_8;
 
+import java.util.Scanner;
+
 public class Q_4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = scanner.nextLine();
+        scanner.close();
+        String revStr = "";
+        for (int i = str.length() - 1; i >= 0; i--) 
+            revStr += str.charAt(i);
         
+        System.out.println("Reversed String is: " + revStr);
     }
 }
